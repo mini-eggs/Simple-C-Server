49d2b82681e613020e8313997ad1ae03b1850e40
Evan M Jones <evan@crowdhubapps.com>
2019-03-25 16:22:27 -0700

    Fix(Cleaning): Just tidying the code up a bit.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #define BUF_MAX 256
 
 int true_sizeof(char *str) {
-    int pos= 0;
+    int pos = 0;
     char curr;
     while((curr = str[pos]) && curr != '\0') {
         pos++;
@@ -24,34 +24,36 @@ int main (int argc, char** argv) {
     }
 
     int port = atoi(argv[1]);
+
     char msg[BUF_MAX];
+    char body[BUF_MAX];
+    char res[BUF_MAX];
 
+    // construct echo message
     strcpy(msg, argv[2]);
-
     for(int i = 3; i < argc; i++) {
         strcat(msg, " ");
         strcat(msg, argv[i]);
     }
 
-    int sockfd, newsockfd, clilen, n;
-    char buf[BUF_MAX];
-    struct sockaddr_in serv_addr, cli_addr;
+    sprintf(res, "HTTP/1.1 200 OK\r\n"
+                 "Content-Type: text/html; charset=utf-8\r\n"
+                 "Content-Length: %d\r\n\r\n%s\r\n", 
+                 true_sizeof(msg), msg);
 
-    char res[BUF_MAX];
-    sprintf(res, "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: %d\r\n\r\n%s\r\n", true_sizeof(msg), msg);
+    int server_fd, client_fd, client_length, status;
+    struct sockaddr_in serv_addr, client_addr;
 
     void handle_exit(){
-        close(newsockfd);
-        close(sockfd);
+        close(client_fd);
+        close(server_fd);
         printf("\n");
         exit(EXIT_SUCCESS);
     }
-
     signal(SIGINT, handle_exit);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if(sockfd < 0) {
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(server_fd < 0) {
         printf("Error opening socket.\n");
         return EXIT_FAILURE;
     }
@@ -62,38 +64,38 @@ int main (int argc, char** argv) {
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
 
-    if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    if(bind(server_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("Error could not bind.\n");
         return EXIT_FAILURE;
     }
 
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
+    listen(server_fd, 5);
+    client_length = sizeof(client_addr);
 
     while(1) {
-        newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
-        if(newsockfd < 0) {
+        client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_length);
+        if(client_fd < 0) {
             printf("Error could not accept.\n");
             return EXIT_FAILURE;
         }
 
         // received
-        bzero(buf, BUF_MAX); // zero out
-        n = read(newsockfd, buf, BUF_MAX);
-        if(n < 0) {
+        bzero(body, BUF_MAX); // zero out
+        status = read(client_fd, body, BUF_MAX);
+        if(status < 0) {
             printf("Error reading from socket.\n");
             return EXIT_FAILURE;
         }
 
         // sending
-        n = write(newsockfd, res, true_sizeof(res)); 
-        if(n < 0) {
+        status = write(client_fd, res, true_sizeof(res)); 
+        if(status < 0) {
             printf("Error writing to socket.\n");
             return EXIT_FAILURE;
         }
 
 
-        close(newsockfd);
+        close(client_fd);
     }
 
     return EXIT_SUCCESS;
